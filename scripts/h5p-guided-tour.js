b512b17a6f690b316d7dbc7844b527821645d8cf
Paal Joergensen <pal.jorgensen@gmail.com>
2017-10-09 10:29:32 +0200

    HFP-1169 Let guided tour destroy itself

--- scripts/h5p-guided-tour.js
+++ scripts/h5p-guided-tour.js
@@ -225,12 +225,12 @@ H5P.GuidedTour = (function ($) {
         catch (err) {} // Suppress error messages / Missing tour elements
 
         // Listen for click-events on body, so we can hide the guide:
-        $('body').on('click.guided-tour', function (event) {
+        $('body').on('click.guided-tour', function () {
           tour.hide();
         });
 
         tour.on('complete', function () {
-          $('body').off('.guided-tour');
+          $('body').off('click.guided-tour');
         });
 
         started();
@@ -251,6 +251,18 @@ H5P.GuidedTour = (function ($) {
      */
     self.hide = function () {
       tour.hide();
+    };
+
+    /**
+     * Destroys tour, i.e removes DOM elements and event listeners
+     */
+    self.destroy = function () {
+      self.hide();
+      $('body').off('click.guided-tour');
+      for (var i = 0; i < tour.steps.length; i++) {
+        var step = tour.steps[i];
+        step && step.destroy();
+      }
     };
 
     /**
